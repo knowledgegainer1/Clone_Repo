67b4388bd23ca11536b354234f0b667c5486757f
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-17 00:58:43 +0530

    added correct code

--- 1st_scriptfile.sh
+++ 1st_scriptfile.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-#
-testing file
-[A[A[[B#[F
-[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[Dt[F[B[Becho ""[DHello!"
+
+#demo
+
+echo "hello"
